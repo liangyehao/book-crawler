0125f37830e0ee9f547596c1e96e7318a0464caa
liangyehao <1094311509@qq.com>
2020-03-16 09:48:07 +0800

    爬虫项目搭建

---
+++ src/main/java/com/liang/bookcrawler/BookCrawlerApplication.java
@@ -0,0 +1,16 @@
+package com.liang.bookcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangyehao
+ */
+@SpringBootApplication
+public class BookCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liang/bookcrawler/BookCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liang.bookcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
