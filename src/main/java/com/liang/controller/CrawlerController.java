1bf02a5bf44eb737be03b86a7a1bdaa340b1e0c8
liangyehao <1094311509@qq.com>
2020-03-20 00:26:22 +0800

    爬虫接口

---
+++ src/main/java/com/liang/controller/CrawlerController.java
@@ -0,0 +1,28 @@
+package com.liang.controller;
+
+import com.liang.utils.HtmlParseUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author liangyehao
+ * @version 1.0
+ * @date 2020/3/18 21:07
+ * @content
+ */
+@RestController
+@RequestMapping("/crawl")
+public class CrawlerController {
+
+    @GetMapping("/{isbn}")
+    public String crawlByIsbn(@PathVariable("isbn") String isbn) throws IOException {
+        String url = "https://book.douban.com/subject/"+isbn;
+        Map<String, String> book = HtmlParseUtil.handleHtml(url);
+        return book.toString();
+    }
+}
